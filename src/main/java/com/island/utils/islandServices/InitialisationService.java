2f2902809de183ae87872e3d0b5e252b84f32cea
Oleksandr Holopapa <sasha.klever12@gmail.com>
2025-07-19 00:25:32 +0300

    add class InitialisationService to fill the island with organisms

---
+++ src/main/java/com/island/utils/islandServices/InitialisationService.java
@@ -0,0 +1,68 @@
+package com.island.utils.islandServices;
+
+import com.island.map.Location;
+import com.island.models.Organism;
+import com.island.models.plants.Plant;
+import com.island.utils.factory.Organisms;
+import com.island.utils.factory.OrganismFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+
+public class InitialisationService {
+    public static void initialize(Location[][] locations) {
+        ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+        for (Location[] locationArray : locations) {
+            for (Location location : locationArray) {
+                executor.submit(()->addOrganismsToLocation(location));
+            }
+        }
+        try {
+            executor.shutdown();
+            boolean awaited = executor.awaitTermination(1, TimeUnit.SECONDS);
+            System.out.println("Initialisation finished: "+awaited);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void addOrganismsToLocation(Location location) {
+        ConcurrentHashMap<String, List<Organism>> organismsInLocation = new ConcurrentHashMap<>();
+        for (Organisms organismEnumValue : Organisms.values()) {
+            //З ймовірністю 60% організм буде в локації
+            if(organismIsPresentInLocation(60)){
+                Organism organism = OrganismFactory.getOrganism(organismEnumValue.toString());
+                List<Organism> identicalOrganismsInLocation = getIdenticalOrganismsList(organism, organismEnumValue);
+                organismsInLocation.put(organism.getClass().getSimpleName(), identicalOrganismsInLocation);
+            }
+        }
+        location.getOrganismsInLocation().putAll(organismsInLocation);
+    }
+
+    private static List<Organism> getIdenticalOrganismsList(Organism organism, Organisms organismEnumValue){
+        List<Organism> identicalOrganismsInLocation = new ArrayList<>();
+        identicalOrganismsInLocation.add(organism);
+        int numberOfIdenticalOrganismsInLocation = getNumberOfIdenticalOrganismsInLocation(organism.getMaxPopulation());
+        //Рослина в списку завжди одна, збільшується тільки її маса
+        if(organism instanceof Plant plant) {
+            double plantWeight = numberOfIdenticalOrganismsInLocation * plant.getWeight();
+            plant.setTotalWeight(plantWeight);
+            plant.setWeight(plantWeight);
+        }
+        else {
+            for (int i = 1; i < numberOfIdenticalOrganismsInLocation; i++) {
+                identicalOrganismsInLocation.add(OrganismFactory.getOrganism(organismEnumValue.toString()));
+            }
+        }
+        return identicalOrganismsInLocation;
+    }
+
+    private static boolean organismIsPresentInLocation(int opportunityToBePresent){
+        return ThreadLocalRandom.current().nextInt(101) <= opportunityToBePresent;
+    }
+
+    private static int getNumberOfIdenticalOrganismsInLocation(int maxPopulation){
+        return ThreadLocalRandom.current().nextInt(1, maxPopulation+1);
+    }
+}
